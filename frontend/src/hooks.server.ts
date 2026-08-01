b84400c784a81e550f6cd2758bc24050a69f1983
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2026-08-01 18:47:30 +0000

    lacales!

---
+++ frontend/src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { paraglideMiddleware } from '$lib/paraglide/server';
+import type { Handle } from '@sveltejs/kit';
+import {getTextDirection} from "$lib/paraglide/runtime";
+
+export const handle: Handle = ({ event, resolve }) => {
+    return paraglideMiddleware(event.request, ({ request: localizedRequest, locale }) => {
+        event.request = localizedRequest;
+
+        return resolve(event, {
+            transformPageChunk: ({ html }) => {
+                return html
+                    .replace('%lang%', locale)
+                    .replace('%dir%', getTextDirection(locale));
+            }
+        });
+    });
+};
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,25 @@
+import {paraglideVitePlugin} from '@inlang/paraglide-js'
 import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-auto';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
 import utwm from 'unplugin-tailwindcss-mangle/vite'
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
+    plugins: [
+        paraglideVitePlugin({
+            project: './project.inlang',
+            outdir: './src/lib/paraglide',
+            emitTsDeclarations: true
+        }),
+        tailwindcss(),
+        sveltekit({
+            compilerOptions: {
+                runes: ({filename}) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+            },
 
-			adapter: adapter()
-		}),
+            adapter: adapter()
+        }),
         utwm()
-	]
+    ]
 });
