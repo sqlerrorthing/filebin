a31eb7dcf3b535300af9dbdde18edec212203f11
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2026-09-11 18:22:31 +0000

    oops

--- frontend/src/lib/components/upload/Upload.svelte
+++ frontend/src/lib/components/upload/Upload.svelte
@@ -223,9 +223,7 @@
     {/if}
 
     <button
-        class={cn("\
-            bg-primary text-primary-foreground \ hover:bg-accent-foreground flex cursor-pointer justify-center gap-3 px-4 py-2
-            shadow-sm sm:w-48",
+        class={cn("bg-primary text-primary-foreground hover:bg-accent-foreground flex cursor-pointer justify-center gap-3 px-4 py-2 shadow-sm sm:w-48",
             loading && "bg-muted-foreground"
         )}
         onclick={() => fileInput?.click()}
