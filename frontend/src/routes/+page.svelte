a28563e3b3bec728d332ea5e658152ac26692491
sqlerrorthing <148702857+sqlerrorthing@users.noreply.github.com>
2026-09-12 21:05:07 +0000

    basics

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
-    import Upload from "$lib/components/upload/Upload.svelte";
-    import JoinShare from "$lib/components/share/JoinShare.svelte";
-    import { limitsStore } from "$lib/stores/limits.svelte";
     import * as m from "$lib/paraglide/messages";
-    import { formatBytes } from "$lib/utils";
+    import ActionButton from "./ActionButton.svelte";
 </script>
 
-<div
-    class="mx-auto flex w-full max-w-md flex-col items-center justify-center
-        gap-4"
->
-    <Upload />
-    <span class="text-muted-foreground text-sm">
-        {m["files.limit-info"]({
-            limit: limitsStore.loading
-                ? "..."
-                : (formatBytes(limitsStore.data?.maxFileSize ?? 0) ?? "Error"),
-        })}
-    </span>
-    <JoinShare />
+<div class="mx-auto flex min-h-screen w-full max-w-7xl md:items-center p-4 sm:p-6 lg:p-8">
+    <div class="flex w-full flex-col gap-4 md:flex-row md:items-center md:justify-between">
+        <div class="flex flex-col gap-4">
+            <h2 class="text-sm text-muted-foreground uppercase">
+                {m["index.subtitle"]()}
+            </h2>
+
+            <h1 class="text-[clamp(46px,7vw,82px)] leading-[.94] tracking-[-.065em] font-bold whitespace-pre-line">
+                {m["index.title"]()}
+            </h1>
+
+            <p class="text-md font-bold text-muted-foreground max-w-lg">
+                {m["index.description"]()}
+            </p>
+        </div>
+
+        <div class="flex gap-2 flex-col">
+            <ActionButton basicKey="index.actions.upload" />
+            <ActionButton basicKey="index.actions.join" />
+        </div>
+
+    </div>
 </div>
---
+++ frontend/src/routes/ActionButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import * as m from "$lib/paraglide/messages";
+
+    let {
+        basicKey
+    }: {
+        basicKey: string
+    } = $props();
+</script>
+
+<button class="w-full sm:w-72" title={m[`${basicKey}.name`]()}>
+    <span>
+        {m[`${basicKey}.name`]()}
+    </span>
+    <span>
+        {m[`${basicKey}.description`]()}
+    </span>
+</button>
